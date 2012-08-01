5f5cccd91628ee4c0b8acde539857692884d2ee2
James C / Pouyan A <dx103@dx103s-iMac.local>
2012-08-01 13:38:59 -0400

    Hannah - put in different messages and responses

--- src/com/hannah/navisimulator/NaviNotifications.java
+++ src/com/hannah/navisimulator/NaviNotifications.java
@@ -24,11 +24,29 @@ public class NaviNotifications {
 
 		PendingIntent ignorePending = PendingIntent.getService(context, 0, ignoreIntent, 0);
 
-		Builder build = new Notification.Builder(context).setContentTitle("Hey, listen!").setTicker("Hey, listen!").setSmallIcon(R.drawable.navi_icon)
-				.addAction(R.drawable.ic_stat_yes_icon, "Yes?", listenPending).setDeleteIntent(ignorePending).setSound(Uri.parse("android.resource://com.hannah.navisimulator/" + R.raw.navi_listen), AudioManager.STREAM_NOTIFICATION);
+		Builder build = new Notification.Builder(context).setContentTitle(context.getString(R.string.hey_listen))
+				.setTicker(context.getString(R.string.hey_listen)).setSmallIcon(R.drawable.navi_icon)
+				.addAction(R.drawable.ic_stat_yes_icon, "Yes?", listenPending).setDeleteIntent(ignorePending)
+				.setSound(Uri.parse("android.resource://com.hannah.navisimulator/" + R.raw.navi_listen), AudioManager.STREAM_NOTIFICATION);
 
 		Notification notification = new Notification.BigPictureStyle(build).bigPicture(
 				BitmapFactory.decodeResource(context.getResources(), R.drawable.navi_glow)).build();
+
+		return notification;
+	}
+
+	public static Notification getListenNotification(Context context, String naviMessage, String response) {
+		Intent ignoreIntent = new Intent();
+		ignoreIntent.setClass(context, NotificationReceiver.class);
+		ignoreIntent.setAction(NotificationReceiver.ACTION_NAVI_IGNORE);
+
+		PendingIntent ignorePending = PendingIntent.getService(context, 0, ignoreIntent, 0);
+
+		Builder build = new Notification.Builder(context).setContentTitle(context.getString(R.string.hey_listen))
+				.setTicker(context.getString(R.string.hey_listen)).setSmallIcon(R.drawable.navi_icon).setDeleteIntent(ignorePending)
+				.addAction(android.R.drawable.ic_delete, response, ignorePending);
+
+		Notification notification = new Notification.BigTextStyle(build).bigText(naviMessage).build();
 
 		return notification;
 	}
--- src/com/hannah/navisimulator/NotificationReceiver.java
+++ src/com/hannah/navisimulator/NotificationReceiver.java
@@ -1,29 +1,30 @@
 package com.hannah.navisimulator;
 
 import java.util.Date;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.IntentService;
-import android.app.Notification;
-import android.app.PendingIntent;
-import android.app.Notification.Builder;
 import android.app.NotificationManager;
 import android.content.Intent;
 
 public class NotificationReceiver extends IntentService {
-
-	private static final int NAVI_REPEAT_MILLIS = 1000 * 20;
 
 	public static String ACTION_NAVI_LISTEN = "com.hannah.navisimulator.NAVI_LISTEN";
 	public static String ACTION_NAVI_IGNORE = "com.hannah.navisimulator.NAVI_IGNORE";
 
 	public static String SWITCH_NOTIFICATION_ARG_NOTIFICATION = "NOTIF_NOTIFICATION";
 
+	private static final int NAVI_REPEAT_MILLIS = 1000 * 20;
+	private static final String[] NAVI_MESSAGES = { "Princess Zelda is in trouble!", "This area is dangerous.", "Go find the Spiritual Stones!" };
+	private static final String[] RESPONSES = { "I know.", "-_-", "Ok?" };
+
+	private Random mRandom;
+
 	public NotificationReceiver() {
-
-		super("NotificationReplaceService");
-
+		super("NaviNotificationService");
+		mRandom = new Random();
 	}
 
 	@Override
@@ -32,16 +33,11 @@ public class NotificationReceiver extends IntentService {
 		final NotificationManager mgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
 		if (ACTION_NAVI_LISTEN.equals(intent.getAction())) {
-			Intent ignoreIntent = new Intent();
-			ignoreIntent.setClass(this, NotificationReceiver.class);
-			ignoreIntent.setAction(NotificationReceiver.ACTION_NAVI_IGNORE);
 
-			PendingIntent ignorePending = PendingIntent.getService(this, 0, ignoreIntent, 0);
+			int messageId = mRandom.nextInt(NAVI_MESSAGES.length);
+			int responseId = mRandom.nextInt(RESPONSES.length);
 
-			Builder build = new Notification.Builder(this).setContentTitle("Hey, listen!").setTicker("Hey, listen!").setSmallIcon(R.drawable.navi_icon)
-					.setDeleteIntent(ignorePending);
-			Notification notification = new Notification.BigTextStyle(build).bigText("Princess Zelda is in trouble!").build();
-			mgr.notify(0, notification);
+			mgr.notify(0, NaviNotifications.getListenNotification(this, NAVI_MESSAGES[messageId], RESPONSES[responseId]));
 
 		} else if (ACTION_NAVI_IGNORE.equals(intent.getAction())) {
 			TimerTask timerTask = new TimerTask() {
@@ -57,5 +53,4 @@ public class NotificationReceiver extends IntentService {
 		}
 
 	}
-
 }
